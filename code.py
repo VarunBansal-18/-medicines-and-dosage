631e35a2eca5718529aeb9f405550a6c2f969455
VarunBansal-18 <varun.25bce10236@vitbhopal.ac.in>
2025-11-22 00:39:21 +0530

    code.py

--- code.py
+++ code.py
@@ -1,24 +1,19 @@
 medicines = []
-
 def add_medicine():
     name = input("Enter medicine name: ")
     dosage = input("Enter dosage (e.g., 1 tablet): ")
     time = input("Enter time to take (e.g., 9 AM): ")
-
     med = {
         "name": name,
         "dosage": dosage,
         "time": time
     }
-
     medicines.append(med)
     print("Medicine added.\n")
-
 def show_medicines():
     if not medicines:
         print("No medicines added yet.\n")
         return
-
     print("\nYour Medicine Schedule:")
     for m in medicines:
         print("Name:", m["name"])
@@ -26,14 +21,11 @@ def show_medicines():
         print("Time:", m["time"])
         print("----------------------")
     print()
-
 while True:
     print("1. Add Medicine")
     print("2. Show All Medicines")
     print("3. Exit")
-
     choice = input("Enter choice: ")
-
     if choice == "1":
         add_medicine()
     elif choice == "2":
